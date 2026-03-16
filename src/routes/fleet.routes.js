521a1841f3acc93be4bc980c2fd85ec3039ab37b
Kennedy Otieno <nyagaotieno@gmail.com>
2026-03-16 08:29:45 -0700

    Added fleet routes

--- src/routes/fleet.routes.js
+++ src/routes/fleet.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getFleetDevices } from "../controllers/fleet.controller.js";
+import { getFleets } from "../controllers/fleet.controller.js";
 import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/devices", requireAuth, getFleetDevices);
+router.get("/devices", requireAuth, getFleets);
 
 export default router;
